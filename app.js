8da34653e9a119eca0179995a00248dd9d6d5a7f
Syndacks <math.milliken@gmail.com>
2017-07-29 16:18:15 -0400

    first commit, get user name using os and fs modules

---
+++ app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
+  if (err) {
+    console.log('Unable to write to file');
+  }
+});

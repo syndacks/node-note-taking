3bf51c60e043ca4abf340d8de357cff819703c43
Syndacks <math.milliken@gmail.com>
2017-07-29 16:57:41 -0400

    Add process arvg logic

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+console.log('Starting app.js');
+
 const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+var command = process.argv[2];
+
+console.log('Command: ', command);
+console.log(process.argv)
+
+if (command === 'add') {
+  console.log('Adding new note.');
+} else if (command === 'list') {
+  console.log('Listing all notes.');
+} else if (command === 'read') {
+  console.log('Reading a note for you.')
+} else if (command === 'remove') {
+  console.log('Removing a note.')
+} else {
+  console.log('Unknown command given.')
+}

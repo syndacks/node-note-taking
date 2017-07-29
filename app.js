b464560ecc76429b2a8d91fc25990ef97e5c5c27
Syndacks <math.milliken@gmail.com>
2017-07-29 16:29:37 -0400

    require a file we create and export function from that file

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
+//
+// var res = notes.addNote();
+// console.log(res);
 
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+var addition = notes.add(9, -2);
+console.log(addition);
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('starting notes.js')
+
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+}

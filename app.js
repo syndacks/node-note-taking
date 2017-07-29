5f8fe2d1bd4e4d5e861a74aff5cfd5f4bff80ef3
Syndacks <math.milliken@gmail.com>
2017-07-29 16:46:31 -0400

    Clean up code before moving on

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes');
-
-// console.log(_.isString(true));
-// console.log(_.isString('Dacks'));
-var filteredArray = _.uniq(['Dacks', 1, 'Dacks', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// var addition = notes.add(9, -2);
-// console.log(addition);
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function (err) {
-//   if (err) {
-//     console.log('Unable to write to file');
-//   }
-// });
